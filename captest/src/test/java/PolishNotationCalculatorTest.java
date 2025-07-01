4be44702bdef215fa5951713aad2d588e50709c5
venkata999 <madhavirah@yahoo.com>
2025-07-01 23:25:57 +0100

    Update PolishNotationCalculatorTest.java

--- captest/src/test/java/PolishNotationCalculatorTest.java
+++ captest/src/test/java/PolishNotationCalculatorTest.java
@@ -2,6 +2,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import com.ven.PolishNotationCalculator;
@@ -9,49 +12,65 @@ import com.ven.exception.PolishNotationException;
 
 public class PolishNotationCalculatorTest {
 
+    private List<String> getListFromString(String str) {
+        return Arrays.asList(str.toString().split(" "));
+    }
+
     @Test
     void rpnCalculate() throws PolishNotationException {
         String calStr = "2 4 * 2 +";
-        assertEquals(10, PolishNotationCalculator.rpnCalculate(calStr));
+        assertEquals(10, PolishNotationCalculator.rpnCalculate(getListFromString(calStr)));
+    }
+
+    @Test
+    void rpnCalculateWithMinus() throws PolishNotationException {
+        String calStr = "12 4 * 2 -";
+        assertEquals(46, PolishNotationCalculator.rpnCalculate(getListFromString(calStr)));
     }
 
     @Test
     void rpnCalculateWithSqrt() throws PolishNotationException {
         String calStr = "10 4 * 2 + 7 + sqrt";
-        assertEquals(7, PolishNotationCalculator.rpnCalculate(calStr));
+        assertEquals(7, PolishNotationCalculator.rpnCalculate(getListFromString(calStr)));
     }
 
     @Test
     void rpnCalculateWithSin() throws PolishNotationException {
         String calStr = "10 4 * 2 + 7 + sin";
-        assertEquals(-0.9537526527594719, PolishNotationCalculator.rpnCalculate(calStr));
+        assertEquals(-0.9537526527594719, PolishNotationCalculator.rpnCalculate(getListFromString(calStr)));
+    }
+
+    @Test
+    void rpnCalculateWithCos() throws PolishNotationException {
+        String calStr = "10 4 * 2 + 7 + cos";
+        assertEquals(+0.3005925437436371, PolishNotationCalculator.rpnCalculate(getListFromString(calStr)));
     }
 
     @Test
     void rpnCalculateWithMod1() throws PolishNotationException {
         String calStr = "10 4 * 9 + 7 mod";
-        assertEquals(0.0, PolishNotationCalculator.rpnCalculate(calStr));
+        assertEquals(0.0, PolishNotationCalculator.rpnCalculate(getListFromString(calStr)));
     }
 
     @Test
     void rpnCalculateWithMod2() throws PolishNotationException {
         String calStr = "10 4 * 9 + 9 mod";
-        assertEquals(4.0, PolishNotationCalculator.rpnCalculate(calStr));
+        assertEquals(4.0, PolishNotationCalculator.rpnCalculate(getListFromString(calStr)));
     }
 
     @Test
     void rpnCalculateWithDivide() throws PolishNotationException {
         String calStr = "55 1 + 9 + 5 /";
-        assertEquals(13.0, PolishNotationCalculator.rpnCalculate(calStr));
+        assertEquals(13.0, PolishNotationCalculator.rpnCalculate(getListFromString(calStr)));
     }
 
     @Test
     void rpnCalculateWithException() {
         String calStr = "1A 4 * 9 + 9 -";
         Exception exception = assertThrows(PolishNotationException.class, () -> {
-            PolishNotationCalculator.rpnCalculate(calStr);
+            PolishNotationCalculator.rpnCalculate(getListFromString(calStr));
         });
-        assertTrue(exception.getMessage().contains("User entered incorrect number"));
+        assertTrue(exception.getMessage().contains("User entered incorrect input"));
 
     }
 
